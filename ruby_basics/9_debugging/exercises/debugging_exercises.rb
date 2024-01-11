71a7d258823b7f86b6d9ebf8eaf81c5da5f296ec
vinay <vinay@disroot.org>
2024-01-11 11:00:30 +0530

    Debugging exercises: pry-byebug

--- ruby_basics/9_debugging/exercises/debugging_exercises.rb
+++ ruby_basics/9_debugging/exercises/debugging_exercises.rb
@@ -1,16 +1,19 @@
-# First, we're going to practice reading the Stack Trace
-# Don't look at this method prior to running the test
-# Type 'rspec' into the terminal to run Rspec
-# Once this test fails, have a look at the Stack Trace
-# Try to see if you can work your way from the last line, the bottom of the stack
-# To the top, the first line, where the error occured, and ONLY THEN fix the error
+require 'pry-byebug'
+=begin
+First, we're going to practice reading the Stack Trace
+ Don't look at this method prior to running the test
+ Type 'rspec' into the terminal to run Rspec
+ Once this test fails, have a look at the Stack Trace
+ Try to see if you can work your way from the last line, the bottom of the stack
+ To the top, the first line, where the error occured, and ONLY THEN fix the error
+=end
 
 def decrement_smallest_value(nested_array)
   smallest_value = nested_array.flatten.max
   nested_array.each do |array|
     array.each do |current_value|
       if smallest_value > current_value
-        smallest_value = current_valu
+        smallest_value = current_value
       end
     end
   end
@@ -24,7 +27,7 @@ def increment_greatest_value(nested_array)
   greatest_value = nested_array.flatten.min
   nested_array.each do |array|
     array.each do |current_value|
-      if greatest_value < nil
+      if greatest_value < current_value
         greatest_value = current_value
       end
     end
@@ -39,7 +42,7 @@ def isogram?(string)
   original_length = string.length
   string_array = string.downcase.split
   unique_length = string_array.uniq.length
-  original_length == unique_length
+  return string_array.uniq.length == unique_length
 end
 
 # Can you guess what's next?
@@ -51,6 +54,6 @@ end
 
 def yell_greeting(string)
   name = string
-  name = name.downcase
+  name = name.upcase
   greeting = "WASSAP, #{name}!"
 end
